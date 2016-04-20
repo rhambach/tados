26cb8b723276a8d42db456ecc5084cbcaeda8314
rhambach <ralf.hambach@uni-jena.de>
2016-04-20 19:44:57 +0200

    adjust max. values for sensitivity analysis of image slicer

--- 07_run_sensitivity_analysis_for_slicer.py
+++ 07_run_sensitivity_analysis_for_slicer.py
@@ -69,33 +69,35 @@ def compensator_rotz(tol,angle):
   
 
 def tilt_obj(tol,xscale=0,yscale=0):   
-  tilt=np.tan(np.deg2rad(5)); # tilt by 5 deg
+  # corresponds to cleave angle of fiber 1
+  tilt=np.tan(np.deg2rad(10)); # tilt by 10 deg
   if xscale<>0: tol.ln.zSetSurfaceParameter(0,1,tilt*xscale)   # set Param1: X TANGENT
   if yscale<>0: tol.ln.zSetSurfaceParameter(0,2,tilt*yscale)   # set Param1: X TANGENT 
   tol.ln.zGetUpdate(); 
   return tilt*xscale,tilt*yscale
   
-def tilt_img(tol,xscale=0,yscale=0):   
-  tilt=np.tan(0.001); # tilt by 1 mrad
+def tilt_img(tol,xscale=0,yscale=0): 
+  # corresponds to cleave angle of fiber 2  
+  tilt=np.tan(np.deg2rad(10)); # tilt by 10 deg
   if xscale<>0: tol.ln.zSetSurfaceParameter(-1,1,tilt*xscale)   # set Param1: X TANGENT
   if yscale<>0: tol.ln.zSetSurfaceParameter(-1,2,tilt*yscale)   # set Param1: X TANGENT 
   tol.ln.zGetUpdate(); 
   return tilt*xscale,tilt*yscale
   
 def decenter_L1(tol,xscale=0,yscale=0): 
-  dcntr=0.020;  # [mm]
+  dcntr=0.002;  # [mm]
   tol.tilt_decenter_elements(1,3,xdec=dcntr*xscale,ydec=dcntr*yscale,
                              cbComment1="decenter L1", cbComment2="~decenter L1");
   return dcntr*xscale,dcntr*yscale
 
 def tilt_L1(tol,xscale=0,yscale=0): 
-  tilt=np.rad2deg(0.001); # [rad]
+  tilt=np.rad2deg(0.002); # [rad]
   tol.tilt_decenter_elements(1,3,xtilt=tilt*xscale,ytilt=tilt*yscale,
                              cbComment1="tilt L1", cbComment2="~tilt L1");
   return tilt*xscale,tilt*yscale
   
 def decenter_L1surf3(tol,xscale=0,yscale=0): 
-  dcntr=0.007; # [mm]
+  dcntr=0.002; # [mm]
   tol.tilt_decenter_surface(3,xdec=dcntr*xscale,ydec=dcntr*yscale);
   return dcntr*xscale,dcntr*yscale
 
@@ -116,7 +118,7 @@ def decenter_L3(tol,xscale=0,yscale=0):
   return dcntr*xscale,dcntr*yscale
 
 def tilt_L3(tol,xscale=0,yscale=0): 
-  tilt=np.rad2deg(0.001); # [rad]
+  tilt=np.rad2deg(0.010); # [rad]
   tol.tilt_decenter_elements(17,19,xtilt=tilt*xscale,ytilt=tilt*yscale,
                              cbComment1="tilt L3", cbComment2="~tilt L3");
   return tilt*xscale,tilt*yscale
@@ -127,12 +129,12 @@ def decenter_F2L3(tol,xscale=0,yscale=0):
   return dcntr*xscale,dcntr*yscale
 
 def tilt_F2L3(tol,xscale=0,yscale=0):
-  tilt=np.rad2deg(0.001); # [rad]
+  tilt=np.rad2deg(0.005); # [rad]
   tol.insert_coordinate_break(17,xtilt=tilt*xscale,ytilt=tilt*yscale,comment="tilt F2L3");  
   return tilt*xscale,tilt*yscale
 
 def decenter_L2(tol,xscale=0,yscale=0): 
-  dcntr=0.020;  # [mm]
+  dcntr=0.010;  # [mm]
   tol.tilt_decenter_elements(13,15,xdec=dcntr*xscale,ydec=dcntr*yscale,
                              cbComment1="decenter L2", cbComment2="~decenter L2");
   return dcntr*xscale,dcntr*yscale
@@ -144,7 +146,7 @@ def tilt_L2(tol,xscale=0,yscale=0):
   return tilt*xscale,tilt*yscale
 
 def tilt_single_mirror(tol,nMirror=1,xscale=0,yscale=0,zscale=0):
-  tilt=np.rad2deg(0.005); # [rad]
+  tilt=np.rad2deg(0.001); # [rad]
   numSurf = tol.get_orig_surface(6);
   pos = tol.ln.zGetNSCPosition(numSurf,nMirror)._asdict();
   pos['tiltX'] += xscale*tilt;
@@ -172,23 +174,40 @@ def tilt_F1(tol,xscale=0,yscale=0):
   return tilt*xscale,tilt*yscale
 
 def tilt_F2(tol,xscale=0,yscale=0):
-  tilt=np.rad2deg(0.001); # [rad]
+  tilt=np.rad2deg(0.005); # [rad]
   tol.insert_coordinate_break(20,xtilt=tilt*xscale,ytilt=tilt*yscale,comment="tilt F1");  
   return tilt*xscale,tilt*yscale      
 
 def decenter_F1(tol,xscale=0,yscale=0):
-  dcntr=0.001; # [mm]
+  dcntr=0.002; # [mm]
   tol.insert_coordinate_break(1,xdec=dcntr*xscale,ydec=dcntr*yscale,comment="decenter F1");  
-  return tilt*xscale,tilt*yscale       
+  return dcntr*xscale,dcntr*yscale       
 
 def decenter_F2(tol,xscale=0,yscale=0):
-  dcntr=0.001; # [mm]
+  dcntr=0.005; # [mm]
   tol.insert_coordinate_break(20,xdec=dcntr*xscale,ydec=dcntr*yscale,comment="decenter F1");  
-  return tilt*xscale,tilt*yscale
+  return dcntr*xscale,dcntr*yscale
        
+def slicer_mirror_separation(tol,dscale=0,xscale=0,yscale=0):
+  thick=0.050; # mm
+  if dscale==0: dscale=xscale;
+  if dscale==0: dscale=yscale;# allow us to use xscale and yscale like in all other functions
+  numSurf = tol.get_orig_surface(6);
+  pos = tol.ln.zGetNSCPosition(numSurf,1)._asdict(); # get M1
+  # shift mirror along global z-axis in order to avoid a change of the referenc point
+  # -> also add chang in y-shift of M1  
+  pos['y'] -= dscale*thick;
+  pos['z'] += dscale*thick;
+  tol.ln.zSetNSCPositionTuple(numSurf,1,**pos)
+  tol.ln.zGetUpdate();
+  return 0,thick*dscale
+
+def optimal(tol,xscale=0,yscale=0):
+  # optimal system without distortion
+  return 0,0;
  
 logging.basicConfig(level=logging.WARNING);
-outpath = os.path.realpath('../03_test_sensitivity_system_14_NA0178');
+outpath = os.path.realpath('../04_sensitivity_analysis_system12_NA0178');
 logfile = os.path.join(outpath,'sensitivity_analysis.txt');  # file for summary of sensitivity analysis
 
 with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
@@ -196,8 +215,8 @@ with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
   # load example file
   #filename = os.path.join(ln.zGetPath()[1], 'Sequential', 'Objectives', 
   #                        'Cooke 40 degree field.zmx')
-  filename= os.path.realpath('../14_catalog_optics_1mm_pupil_inf-inf-relay_point_source_with_slicer_tolerancing.ZMX');
-  #filename= os.path.realpath('../12_catalog_optics_1mm_pupil_point_source_with_slicer_tolerancing.ZMX');  
+  #filename= os.path.realpath('../13_catalog_optics_1mm_pupil_inf-inf-relay_point_source_with_slicer_tolerancing.ZMX');
+  filename= os.path.realpath('../12_catalog_optics_1mm_pupil_point_source_with_slicer_tolerancing.ZMX');  
   tol=ToleranceSystem(hDDE,filename)
 
   # define list of tolerances:
@@ -222,12 +241,17 @@ with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
                 tilt_F2,
                 decenter_F1,
                 decenter_F2,
+                slicer_mirror_separation,
+                optimal
                 ];
 
+  OUT.write('system: %s\n'%filename)
+  
   # for each possible disturbation of the optical system
   for itol,disturb_func in enumerate(tolerances):
     disturb_name = disturb_func.func_name;
     pklfile = os.path.join(outpath,'run%04d_%s.pkl'%(itol,disturb_name));
+    if os.path.exists(pklfile): continue;  # do not overwrite existing data
     print pklfile;
     OUT.write('\n-- %10s -----------------------------------------------\n'%disturb_name);
     OUT.write('  pklfile: %s\n'%pklfile);    
@@ -241,7 +265,7 @@ with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
       save['scalexy'].append((xscale,yscale));    
       
       # allow for compensators, here rotation about surface normal of slicer
-      fig1,(ax1,ax2) = plt.subplots(nrows=1,ncols=2,sharey=True);   
+      # fig1,(ax1,ax2) = plt.subplots(nrows=1,ncols=2,sharey=True);   
       for rotz in (0,):#0.5,1):
         save['rotz'].append(rotz);
     
@@ -253,8 +277,7 @@ with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
         # disturb system
         xs,ys = disturb_func(tol,xscale=xscale,yscale=yscale);
         save['dxy'].append((xs,ys));
-        #print "tolerance '%20s': dx=%8.5f, dy=%8.5f, dr=%8.5f"%(disturb_name,xs,ys,np.linalg.norm((xs,ys)))
-           
+        
         # update changes
         tol.ln.zPushLens(1);    
         if rotz==0: tol.print_current_geometric_changes();
@@ -289,10 +312,11 @@ with DDElinkHandler() as hDDE, open(logfile,'w') as OUT:
         y_boxy,I_boxy = intensity_in_box(y,cumy,min_width=0.03,max_width=0.04);
       
         # update figure 
-        ax1.plot(x_boxx*1000,I_boxx,label='rotz=%3.1f'%rotz);
-        ax2.plot(y_boxy*1000,I_boxy,label='rotz=%3.1f'%rotz);
+        # ax1.plot(x_boxx*1000,I_boxx,label='rotz=%3.1f'%rotz);
+        # ax2.plot(y_boxy*1000,I_boxy,label='rotz=%3.1f'%rotz);
       
         # log results
+        print "tolerance '%20s': dx=%8.5f, dy=%8.5f, Itot=%8.5f"%(disturb_name,xs,ys,Itot)
         OUT.write('  %8.5f  %8.5f  %8.5f  %8.5f  %8.5f\n'%(xs,ys,np.linalg.norm((xs,ys)),rotz,Itot));
       
       # end: for rotz      
