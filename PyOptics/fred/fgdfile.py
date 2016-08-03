d8a1664d4c18ffd6b4edc957a26c1137032cb6f1
Uwe Lippmann <uwe.lippmann@iof.fraunhofer.de>
2016-08-03 18:08:30 +0200

    Added docstring, removed "__future__" imports and "print" calls.

--- PyOptics/fred/fgdfile.py
+++ PyOptics/fred/fgdfile.py
@@ -5,12 +5,38 @@ Created on Mon Jul 11 15:51:05 2016
 @author: ulip
 """
 
-from __future__ import print_function, division
 import numpy as np
 
 
 def from_fgd(f, header_info=False):
+    """
+    Construct an array from data in a FRED FGD file.
     
+    Parameters
+    ----------
+        f : file or str
+            Open the file object or filename.
+        header_info : bool
+            If ``True`` returns a dict with the contents of the header of the file
+            along with the actual data array.
+    
+    Example
+    -------
+        >>> import fred.fgdfile
+        >>> data, header = fred.fgdfile.from_fgd("datafile.fgd", header_info=True)
+        >>> header
+        {'ORIGIN_POSITION': array([ -34.86855408,  -98.8746984 ,  171.25600122]),
+        'TITLE': '', 'B_AXIS_MIN': -2.7135, 'A_AXIS_MAX': 3.392, 'B_AXIS_TYPE': 'Spatial',
+        'A_AXIS_TYPE': 'Spatial', 'DATATYPE': 'Double', 'B_AXIS_DIM': 1024, 'DATAUNITS': '',
+        'A_AXIS_DIR': array([ 0.98480775, -0.08682409,  0.15038373]), 'A_AXIS_DIM': 1280,
+        'B_AXIS_DIR': array([ 0.       ,  0.8660254,  0.5      ]), 'B_AXIS_LABEL': 'Cam Height',
+        'PHYSICAL_MEANING': 'Unknown', 'FILETYPE': 'Grid2D',
+        'DATETIME': 'Friday, July 15, 2016 14:15:38',
+        'VERSION': '2', 'BINARY': True, 'HOLE_VALUE': 1e+308, 'B_AXIS_MAX': 2.7135,
+        'FRED_FILENAME': 'optisches_modell.frd', 'A_AXIS_MIN': -3.392, 'A_AXIS_UNITS': 'mm',
+        'A_AXIS_LABEL': 'Cam Width', 'B_AXIS_UNITS': 'mm'}
+    """
+
     if type(f) == str:
         f = open(f, "r")
     
@@ -18,7 +44,6 @@ def from_fgd(f, header_info=False):
     line = f.readline()
     if line.rstrip().upper() != "FRED_DATA_FILE":
         f.close()
-        print("File is not a FRED FGD file.")
         raise IOError
     
     info = {}
@@ -111,11 +136,3 @@ def from_fgd(f, header_info=False):
         return data, info
     else:
         return data
-
-
-if __name__ == "__main__":
-    
-    f = "Camera 1_integral.fgd"
-    data, info = from_fgd(f, header_info=True)
-    print(info)
-    
